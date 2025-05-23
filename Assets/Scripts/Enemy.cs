848b7403eede4e4b9930f7ab5183565d84d2aa6d
Jonas Molsted <jonasmolsted@gmail.com>
2025-05-23 15:59:47 +0200

    mid waves

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -33,7 +33,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("count: " +  enemyCount);
         if (gameObject != null)
         { 
             enemyAgent.SetDestination(playerTransform.position); 
--- Assets/Scripts/Waves.cs
+++ Assets/Scripts/Waves.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Waves : MonoBehaviour
 {
-    public delegate void WaveDelegate(int _waveNumber);
+    public delegate void WaveDelegate(int _waveNumber, int _midWaveSpawn = 1, string _waveType = "wave" );
     public event WaveDelegate waveEvent;
     public int waveNumber;
+    [SerializeField] private EnemySpawner enemySpawner;
+    private int midWaySpawn;
 
     void Start()
     {
         Enemy.deathEvent += HandleEnemyDeath; // because static event, no enemy instance reference needed (enemy instance is passed along with broadcast)
         waveNumber = 1;
         waveEvent?.Invoke( waveNumber );
+        enemySpawner.allEnemiesSpawnedEvent += HandleAllEnemiesSpawned;
     }
 
     void Update()
@@ -22,6 +25,14 @@ public class Waves : MonoBehaviour
         if( Enemy.enemyCount == 1 ) {
             waveNumber++;
             waveEvent?.Invoke( waveNumber );
+            return;
         }
+        if( Enemy.enemyCount <= midWaySpawn ) {
+            waveEvent?.Invoke( waveNumber, midWaySpawn, "MidWave" );
+        }
+    }
+
+    private void HandleAllEnemiesSpawned( int spawnAmount, float enemiesRemaining01 ) {
+        midWaySpawn = Mathf.RoundToInt( enemiesRemaining01 * spawnAmount );
     }
 }
