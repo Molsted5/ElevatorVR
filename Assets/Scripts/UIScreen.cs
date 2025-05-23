35c9d1751003c2c362cdf65ddf5aff3cd5a213f3
johanne <johanne@ornsovej.dk>
2025-05-23 16:35:33 +0200

    UI with score

--- Assets/Scripts/UIScreen.cs
+++ Assets/Scripts/UIScreen.cs
@@ -3,19 +3,22 @@ using TMPro;
 
 public class UIScreen : MonoBehaviour
 {
-    public TMP_Text ScoreandHealth;
+    public TMP_Text TextUI;
     public Player playerscript;
+    public ScoreManager scoreScript;
     int playerHealth;
+    int playerScore;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerHealth = playerscript.playerHealth;
+        playerHealth = playerscript.currentHealth;
+        playerScore = scoreScript.currentScore;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ScoreandHealth.text = "Current health: " + playerHealth.ToString();
+        TextUI.text = "Current health: " + playerHealth.ToString() + "<br><br>" + "Current Score: " + playerScore.ToString();
     }
 }
