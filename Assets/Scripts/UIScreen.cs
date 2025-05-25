20163803146d790c614d5637f140f63ddc3fbe3e
Emil <emilh23@student.sdu.dk>
2025-05-25 13:30:56 +0200

    Fixed UI screen

--- Assets/Scripts/UIScreen.cs
+++ Assets/Scripts/UIScreen.cs
@@ -4,24 +4,46 @@ using TMPro;
 public class UIScreen : MonoBehaviour
 {
     public TMP_Text TextUI;
-    public Player playerscript;
+    public Player playerScript;
     public ScoreManager scoreScript;
+    public Waves waveScript;
     int playerHealth;
     int playerScore;
+    int waveNr;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        UpdateScreen();
 
-  
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
       
-        playerHealth = playerscript.currentHealth;
-        playerScore = scoreScript.currentScore;
-        TextUI.text = "Current health: " + playerHealth.ToString() + "<br><br>" + "Current Score: " + playerScore.ToString();
+
+        
     }
+
+    public void UpdateScreen()
+    {
+        waveNr = waveScript.waveNumber;
+        playerHealth = playerScript.currentHealth;
+        playerScore = scoreScript.currentScore;
+        TextUI.text = 
+            "Wave: " + waveNr.ToString() + "<br><br>" + 
+            "Current health: " + playerHealth.ToString() + "<br><br>" + 
+            "Current Score: " + playerScore.ToString();
+
+
+    }
+
+ 
+
+
+
 }
